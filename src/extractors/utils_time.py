56c172e56568dabb0d77edf7489f8dcfd1ad417c
adamMedwards <adamedwards6989@iejesusmirey.com>
2025-11-09 04:41:54 -0800

    feat: add src/extractors/utils_time.py

---
+++ src/extractors/utils_time.py
@@ -0,0 +1,5 @@
+thonfrom datetime import datetime
+
+def format_time(time_string):
+    # Convert time string like "2 days ago" to a datetime object
+    return datetime.now()  # Dummy implementation for now
